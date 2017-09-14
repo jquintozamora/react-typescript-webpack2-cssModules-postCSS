17b1d766f01447fc504ec805e17dd56aab929eee
jquintozamora <jose.quinto.zamora@arcadiagroup.co.uk>
2017-09-14 22:08:14 +0100

    add enzyme, enzyme-to-json, identity-obj-proxy, jest, ts-jest, react-test-renderer and jest configuration in package.json

---
+++ test/unit/helpers/ComponentHelper.tsx
@@ -0,0 +1,25 @@
+import { shallow, mount, render } from 'enzyme';
+import * as React from 'react';
+
+const renderComponentHelper = (Component: any, mountOptions = {}) => {
+
+    return ({ children = null, ...props } = {}) => {
+        const wrapper = shallow(
+            <Component {...props } > {children} </Component>,
+            { ...mountOptions }
+        );
+
+        const component = {
+            wrapper,
+            instance: wrapper.instance(),
+            //requires "snapshotSerializers": ["enzyme-to-json/serializer"] in jest config to extract just the html
+            getHtml: () => wrapper
+        }
+
+        return {
+            ...component,
+        }
+    }
+}
+
+export default renderComponentHelper;
