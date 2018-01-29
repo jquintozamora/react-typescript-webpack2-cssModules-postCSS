4bdb345215daa23f7fb126a3b191cfcb31058a99
jquintozamora <jose.quinto.zamora@arcadiagroup.co.uk>
2018-01-29 16:41:48 +0000

    solve some issues for  macOS

--- webpack/addons/webpack.bundleVisualizer.js
+++ webpack/addons/webpack.bundleVisualizer.js
@@ -7,13 +7,14 @@
 ///////////////////////////////////////////////////////////
 
 const Visualizer = require('webpack-visualizer-plugin');
+const path = require('path')
 const commonPaths = require("../common-paths");
 
 const currentDateTime = new Date();
 const currentDate = currentDateTime.toLocaleDateString('en-GB').replace(/\//g, "-");
 const currentTime = currentDateTime.toLocaleTimeString('en-GB', { hour12: false }).replace(/:/g, "-");
 const fileDateTime = currentDate + "-" + currentTime;
-const filename = commonPaths.bundleVisualizerStatsPath + "\\statistics-" + fileDateTime + ".html";
+const filename = path.join(commonPaths.bundleVisualizerStatsPath, "statistics-" + fileDateTime + ".html");
 console.log("Generating stats html file here: " + filename);
 module.exports = {
     plugins: [
