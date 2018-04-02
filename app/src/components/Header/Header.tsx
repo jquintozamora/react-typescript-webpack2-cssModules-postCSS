389d55d444002b6dd3f56c839ac096d3b2f1fab4
Jose Quinto <quintorel@hotmail.com>
2018-04-02 14:20:20 +0100

    update verions

--- app/src/components/Header/Header.tsx
+++ app/src/components/Header/Header.tsx
@@ -3,15 +3,14 @@ import * as styles from './Header.module.css'
 import { IHeaderProps } from './IHeaderProps'
 
 class Header extends React.Component<IHeaderProps, {}> {
-
-    public render() {
-        return (
-            <header className={styles.header}>
-                <h1>Hello, {this.props.title}</h1>
-                <h3>Header background Color from PostCSS => var(--headerBackground): {styles.header_bg}</h3>
-            </header>
-        )
-    }
+  public render() {
+    return (
+      <header className={styles.header}>
+        <h1>Hello, {this.props.title}</h1>
+        <h3>Header background Color from PostCSS => var(--headerBackground): {styles.header_bg}</h3>
+      </header>
+    )
+  }
 }
 
 export default Header
